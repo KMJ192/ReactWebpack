34710a5d12477b9eb834cac94b87cda5d2d580fe
MyeongJunKim <myeongjun222@gmail.com>
2022-05-14 22:38:46 +0900

    jest config 파일 수정

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,11 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)?$': 'babel-jest',
   },
   moduleNameMapper: {
-    '^@src': '<rootDir>/src',
+    '^@/(.*)$': '<rootDir>/$1',
+    '@src(.*)$': '<rootDir>/src/$1',
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.js',
   },
+  testEnvironment: 'jsdom',
 };
